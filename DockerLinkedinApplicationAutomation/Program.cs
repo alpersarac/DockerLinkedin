a074fdd78035406720ca32af8a2b3b32b57cf8e4
alpersarac <alper.sarac42@gmail.com>
2023-06-22 22:59:56 +0300

    Add project files.

---
+++ DockerLinkedinApplicationAutomation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DockerLinkedinApplicationAutomation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i<1000)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(i+".Step");
+                i++;
+            }
+        }
+    }
+}
